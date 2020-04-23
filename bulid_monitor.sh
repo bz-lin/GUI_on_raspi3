a08e03f32a00bfdc5f82c684f57360da82eeb740
bz-lin <56818112+bz-lin@users.noreply.github.com>
2020-04-23 11:22:55 +0800

    Update bulid_monitor.sh

--- bulid_monitor.sh
+++ bulid_monitor.sh
@@ -29,22 +29,24 @@
 #echo "write_enable=Yes" >> /etc/vsftpd.conf
 
 ##?install kivy ver.1.11.1?##
-apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
-pkg-config libgl1-mesa-dev libgles2-mesa-dev \
-python-setuptools libgstreamer1.0-dev git-core \
-gstreamer1.0-plugins-{bad,base,good,ugly} \
-gstreamer1.0-{omx,alsa} python-dev libmtdev-dev \
-xclip xsel libjpeg-dev \
-libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev python3-pip
+#apt install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
+#pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+#python-setuptools libgstreamer1.0-dev git-core \
+#gstreamer1.0-plugins-{bad,base,good,ugly} \
+#gstreamer1.0-{omx,alsa} python-dev libmtdev-dev \
+#xclip xsel libjpeg-dev \
+#libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev python3-pip
 
-pip3 install setuptools
-pip3 install Cython
+#pip3 install setuptools
+#pip3 install Cython
 #wget https://kivy.org/downloads/1.11.1/Kivy-1.11.1.tar.gz
-#wget https://kivy.org/downloads/1.11.1/Kivy_examples-1.11.1-py2.py3-none-any.whl
-#pip3 install Kivy_examples-1.11.1-py2.py3-none-any.whl
 #tar -xvaf Kivy-1.11.1.tar.gz
 #cd  Kivy-1.11.1
 #pip3 install .
+
+#add touchscreen setting
+#sed -i '/^\[input\]$/a\mtdev_%(name)s = probesysfs,provider=mtdev' /home/pi/.kivy/config.ini
+#sed -i '/^\[input\]$/a\hid_%(name)s = probesysfs,provider=hidinput' /home/pi/.kivy/config.ini
 
 
 ##install raspap##
