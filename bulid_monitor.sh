51a02671348fcc3874a4018a330037f0fa5928a5
bz-lin <56818112+bz-lin@users.noreply.github.com>
2020-04-23 11:34:51 +0800

    Update bulid_monitor.sh

--- bulid_monitor.sh
+++ bulid_monitor.sh
@@ -1,28 +1,28 @@
 ### build monitor with raspberry buster lite img ###
 
 ##enable ssh service##
-#systemctl enable ssh
-#systemctl restart ssh
+systemctl enable ssh
+systemctl restart ssh
 
 
 ##chage hostname##
-#hostnamectl set-hostname monitor
+hostnamectl set-hostname monitor
 
 ##change password for pi##
-#echo pi:monitor | chpasswd
+echo pi:monitor | chpasswd
 
 ##Rotate touchscreen, disable onboard wifi bluetooth##
-#echo "lcd_rotate=2" >> /boot/config.txt
-#echo "dtoverlay=pi3-disable-wifi" >> /boot/config.txt
-#echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
-#systemctl disable hciuart
+echo "lcd_rotate=2" >> /boot/config.txt
+echo "dtoverlay=pi3-disable-wifi" >> /boot/config.txt
+echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
+systemctl disable hciuart
 
 ##change mirror for raspi##
-#http://free.nchc.org.tw/raspbian/raspbian
-#echo "deb http://free.nchc.org.tw/raspbian/raspbian/ buster main contrib non-free rpi" > /etc/apt/sources.list     
+http://free.nchc.org.tw/raspbian/raspbian
+echo "deb http://free.nchc.org.tw/raspbian/raspbian/ buster main contrib non-free rpi" > /etc/apt/sources.list     
 
 ##update for raspi##
-#apt update
+apt update
 
 ##install vsftpd.service & setting##
 #apt install vsftpd -y
